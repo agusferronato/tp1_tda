321ff176a7826c3979adccbe3d26497a4ea92a0b
agusferronato <aferronato@fi.uba.ar>
2025-09-07 16:36:21 -0300

    manejo de errores

--- tp1.py
+++ tp1.py
@@ -8,6 +8,11 @@ def orden_batallas (ti, bi):
 
 
 if __name__ == "__main__":
-    ruta = sys.argv[1]
-    bi, ti = obtener_bi_ti(ruta)
-    print(orden_batallas(ti, bi))
+    try:
+        ruta = sys.argv[1]
+        bi, ti = obtener_bi_ti(ruta)
+        print(orden_batallas(ti, bi))
+    except (FileNotFoundError):
+        print("Error: no se ha encontrado el archivo")
+    except (IndexError):
+        print("Error: Debe pasar como primer parametro el nombre del archivo")
