1d20a14db6b76b5df75f4a65abaa7e499a91a232
xFedePumx <fparsons@fi.uba.ar>
2025-09-07 17:48:36 -0300

    procesamos los datos

--- tp1.py
+++ tp1.py
@@ -1,5 +1,18 @@
 import sys
+import csv
+
+
+def parser(ruta: str) -> list[tuple[int, int]]:
+    datos: list = []
+    with open(ruta, "r") as archivo:
+        lector = csv.reader(archivo)
+        next(lector)  # Saltear el encabezado
+        for fila in lector:
+            datos.append((int(fila[0]), int(fila[1])))
+    return datos
+
 
 if __name__ == "__main__":
-    ruta = sys.argv[1]
-    # Codigo...
+    datos: list = parser(sys.argv[1])
+    if not datos:
+        print("No se encontraron los datos.")
