754bf333f3f0fb18ffc5a52f646b3d2240a21129
xFedePumx <fparsons@fi.uba.ar>
2025-09-16 16:49:27 -0300

    falla algo de import de un modulo

--- complejidad/varianza.py
+++ complejidad/varianza.py
@@ -5,9 +5,10 @@ import seaborn as sns
 from utils.generar_sets_datos import get_random_array
 from complejidad.grafico import graficar
 from tp1 import orden_batallas
+from complejidad.util import time_algorithm
 
 MUESTRAS: int = 5
-ESCALAR: int = 10e10
+ESCALAR: int = 3000
 SIZE: int = 1000
 
 
@@ -33,10 +34,27 @@ def ambos_grande():
     return contrucion_del_set(ESCALAR, ESCALAR)
 
 
+casos = [
+    ("ambos nomrales", ambos_normales),
+    ("tiempos grandes", t_grande),
+    ("pesos grandes", b_grande),
+    ("ambos grandes", ambos_grande)
+]
+
+resultados = []
+
+for nombre, generador in casos:
+    tiempos_dict = time_algorithm(orden_batallas, [SIZE], generador)
+    tiempo_promedio = tiempos_dict[SIZE]
+    resultados.append((nombre, tiempo_promedio))
+
+for nombre, tiempo in resultados:
+    plt.bar(nombre, tiempo, label=nombre)
+
 plt.title("Comparacion de tiempos de ejecucion segun los numeros")
-plt.xlabel("")
-plt.ylabel("tiempo de ejecucion (s)")
+plt.xlabel("Caso")
+plt.ylabel("Tiempo de ejecucion (s)")
 plt.legend()
-plt.grid(true)
+plt.grid(True)
 plt.tight_layout()
 plt.show()
