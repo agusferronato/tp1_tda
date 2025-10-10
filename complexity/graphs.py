89be91ba4a2738ef8c499f6a1506d55b19e32e8d
agusferronato <aferronato@fi.uba.ar>
2025-10-10 14:01:43 -0300

    cambio logica complejidad

--- complexity/graphs.py
+++ complexity/graphs.py
@@ -14,52 +14,56 @@ from utils.data_sets import generate_data_set
 from utils.files import get_file_info_by_size
 
 
-SAMPLES = 25
-N_MIN = 1000
-N_MAX = 12500
+SAMPLES = 10
+N_MIN = 50
+N_MAX = 1500
 
 
 def square(n, c1, c2):
-    return c1 * n * n + c2
+    return c1 * n ** 2 + c2
+
+
+def cubed(n, c1, c2):
+    return c1 * n ** 3 + c2
+
+
+def nlogn(n, c1, c2):
+    return c1 * n * np.log(n) + c2
+
+
+
 
 
 def get_results(sizes, fn):
     return time_algorithm(algorithm, sizes, lambda s: fn(s))
 
 
-def quadratic_error(results, c, x):
-    return np.sum((c[0] * x * x + c[1] - [results[n] for n in x])**2)
+def quadratic_error(results, c, x, function):
+    return np.sum((function(x, c[0], c[1]) - [results[n] for n in x])**2)
 
 
-def graph(x, results, error=False):
+
+
+
+def graph_time(x, function, results):
 
     ax: plt.Axes
     fig, ax = plt.subplots()
 
-    c, pcov = sp.optimize.curve_fit(square, x, [results[n] for n in x])
+    c, pcov = sp.optimize.curve_fit(function, x, [results[n] for n in x])
 
-    if error:
-        errors = [np.abs(c[0] * n * n + c[1] - results[n]) for n in x]
+    
+    ax.scatter(x, [results[i] for i in x], label="Medición")
+    ax.set_title('Tiempo de ejecución del algoritmo')
+    ax.set_xlabel('Tamaño del array')
+    ax.set_ylabel('Tiempo de ejecución (s)')
 
-        ax.plot(x, errors)
-
-        ax.set_title('Error de ajuste')
-        ax.set_xlabel('Tamaño del array')
-        ax.set_ylabel('Error absoluto (s)')
-
-    else:
-
-        ax.scatter(x, [results[i] for i in x], label="Medición")
-        ax.set_title('Tiempo de ejecución del algoritmo')
-        ax.set_xlabel('Tamaño del array')
-        ax.set_ylabel('Tiempo de ejecución (s)')
-
-        ax.plot(x, [c[0] * n * n + c[1] for n in x], 'r--', label="Ajuste")
-        ax.legend()
-        fig
+    ax.plot(x, [function(n, c[0], c[1]) for n in x], 'r--', label="Ajuste")
+    ax.legend()
+    fig
 
 
-    r = quadratic_error(results, c, x)
+    r = quadratic_error(results, c, x, function)
 
     print(f"c_1 = {c[0]}, c_2 = {c[1]}")
     print(f"Error cuadrático total: {r}")
@@ -68,15 +72,39 @@ def graph(x, results, error=False):
 
 
 
-def graph_time(sizes, fn):
 
-    results = get_results(sizes, fn)
-    graph(sizes, results)  
-    graph(sizes, results, error=True)  
+def graph_error(x, functions, results):
+
+    ax: plt.Axes
+    fig, ax = plt.subplots()
+    
+
+    for function_name, function in functions.items():
+
+        c, _ = sp.optimize.curve_fit(function, x, [results[n] for n in x])
+
+        errors_function = [np.abs(function(n, c[0], c[1]) - results[n]) for n in x]
+
+        ax.plot(x, errors_function, label=f"Ajuste ${function_name}$")
+
+        print(f"Error cuadrático total para {function_name}: {np.sum(np.power(errors_function, 2))}")
+
+    ax.set_title('Error de ajuste')
+    ax.set_xlabel('Tamaño del array')
+    ax.set_ylabel('Error absoluto (s)')
+    ax.legend()
+
+    plt.show()
 
 
 
 if __name__ == '__main__':
+
+    functions = {
+        "n^2": square,
+        "n^3": cubed,
+        "nlogn": nlogn
+    }
 
     sns.set_theme()
 
@@ -85,4 +113,11 @@ if __name__ == '__main__':
     for size in sizes:
         generate_data_set(size)
 
-    graph_time(sizes, get_file_info_by_size)
+    fn = get_file_info_by_size
+
+    results = get_results(sizes, fn)
+
+    for _, function in functions.items():
+        graph_time(sizes, function, results)
+
+    graph_error(sizes, functions, results)
