530f4cf00256541b909eb22feff02c498305abf7
xFedePumx <fparsons@fi.uba.ar>
2025-11-14 14:47:13 -0300

    para generar sets grande que sabes el optimos sin usar bt

--- utils/generate_data_sets.py
+++ utils/generate_data_sets.py
@@ -19,7 +19,7 @@ EXPECTED_RESULTS_FILE = "Resultados Esperados.txt"
 EXPECTED_RESULTS_PATH = os.path.join(FOLDER, EXPECTED_RESULTS_FILE)
 
 
-def generate_data_sets (size, k):
+def generate_data_setsS(size, k):
 
     masters = []
     lines = []
@@ -30,11 +30,9 @@ def generate_data_sets (size, k):
             master_power = random.randint(MIN_POWER, MAX_POWER)
             masters.append(("", master_power))
             file.write(f", {master_power}\n")
-        
 
     if not os.path.exists(EXPECTED_RESULTS_PATH):
         open(EXPECTED_RESULTS_PATH, "w").close()
-
 
     with open(EXPECTED_RESULTS_PATH, "r") as file:
         lines = file.readlines()
@@ -45,8 +43,8 @@ def generate_data_sets (size, k):
             if lines[i].startswith(f"{size}_{k}.txt"):
                 lines[i + 1] = f"{optimal_value}\n"
                 file_found = True
-                break 
-            
+                break
+
         if not file_found:
             lines.append("\n")
             lines.append(f"{size}_{k}.txt\n")
@@ -56,5 +54,18 @@ def generate_data_sets (size, k):
     with open(EXPECTED_RESULTS_PATH, "w+") as file:
         file.writelines(lines)
 
-    
 
+def generate_data_sets_worst_pakku(k_2: int):
+    k: int = k_2 * 2
+    with open(f"{FOLDER}/worst_pakku_{k}.txt", "w") as file:
+        file.write(f"\n{k}\n")
+        for i in range(1, k+1):
+            file.write(f", {2*k - i}\n")
+            file.write(f",{2*k - i}\n")
+        file.write(f", {k}\n")
+
+    expected_result: int = 9 * (k ** 3)
+    with open(EXPECTED_RESULTS_PATH, "a") as file:
+        file.write("\n")
+        file.write(f"worst_pakku_{k}.txt\n")
+        file.write(f"{expected_result}\n")
